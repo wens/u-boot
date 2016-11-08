1755a93d26b25519fa04ebfc7c34c16235c24fe0
Chen-Yu Tsai <wens@csie.org>
2016-11-08 14:40:25 +0800

    sunxi: Use secure SRAM B for secure RAM for sun9i/A80
    The A80 has a 256 kiB secure SRAM. However the first 4 kiB are reserved
    for CPU0 hotplug flags.
    
    Signed-off-by: Chen-Yu Tsai <wens@csie.org>

--- include/configs/sun9i.h
+++ include/configs/sun9i.h
@@ -13,6 +13,10 @@
  * A80 specific configuration
  */
 
+/* First 4 kiB is reserved for CPU0 hotplug flags */
+#define CONFIG_ARMV7_SECURE_BASE	(SUNXI_SRAM_B_BASE + 4 * 1024)
+#define CONFIG_ARMV7_SECURE_MAX_SIZE	(252 * 1024) /* 252 KB */
+
 /*
  * Include common sunxi configuration where most the settings are
  */
